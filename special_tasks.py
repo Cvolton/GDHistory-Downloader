d7024460cefef62d30c1d05c85801c780c3ffa82
Cvolton <cvolton@cvolton.eu>
2022-05-27 14:29:05 +0200

    Fix recursion

--- special_tasks.py
+++ special_tasks.py
@@ -4,22 +4,21 @@ import server_parsers
 import time
 
 def get_id_range_task(start, finish):
-	request_delay = utils.get_request_delay()
-	print(f"[{start}/{finish}]")
-	ids = []
-	end = finish if finish < (start+100) else start+100
-	for i in range(start, end):
-		ids.append(str(i))
-	if not ids:
-		return
-	request_str = ",".join(ids)
-	response_text = utils.save_request('getGJLevels21', {"type": 19, "str": request_str} )
+	while True:
+		request_delay = utils.get_request_delay()
+		print(f"[{start}/{finish}]")
+		ids = []
+		end = finish if finish < (start+100) else start+100
+		for i in range(start, end):
+			ids.append(str(i))
+		if not ids:
+			return
+		request_str = ",".join(ids)
+		response_text = utils.save_request('getGJLevels21', {"type": 19, "str": request_str} )
 
-	new_start = 0
-	if response_text is not False:
-		last_level = server_parsers.response_to_dict(response_text.split('#')[0].split('|')[-1], ':')
-		new_start = int(last_level[1]) + 1
-	else:
-		new_start = end
-	time.sleep(request_delay)
-	get_id_range_task(new_start, finish)
+		if response_text is not False:
+			last_level = server_parsers.response_to_dict(response_text.split('#')[0].split('|')[-1], ':')
+			start = int(last_level[1]) + 1
+		else:
+			start = end
+		time.sleep(request_delay)
