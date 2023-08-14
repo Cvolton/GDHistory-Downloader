c7b7edb1106903e6cd080c7a2b339ac63afa27db
Cvolton <cvolton@cvolton.eu>
2023-08-14 09:14:25 +0200

    print response content on failure

--- utils.py
+++ utils.py
@@ -76,6 +76,7 @@ def send_request(endpoint, data, *args, **kwargs):
 
 	if (response.text == '' or response.status_code != 200) and attempt < 4:
 		print(f"Being rate-limited, sleeping... {response.status_code}")
+		print(f"Response content: {response.text}")
 		time.sleep(60*attempt)
 		return send_request(endpoint, data, attempt=attempt+1)
 
