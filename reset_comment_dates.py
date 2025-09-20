0327533186c1906472d84f88aee6a1934dea9395
Cvolton <cvolton@cvolton.eu>
2025-09-20 18:14:48 +0200

    add reset_comment_dates

---
+++ reset_comment_dates.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import utils
+
+comment_json_filename = sys.argv[1] if len(sys.argv) > 1 else f"comments_by_date_updated.json"
+
+with open(f"{utils.get_data_path()}/{comment_json_filename}.json", "r") as f:
+    all_comments = json.loads(f.read())
+    
+all_comments_flattened = {}
+all_comments_remaster = {}
+for date_bracket, comments in all_comments.items():
+    for comment_id, comment in comments.items():
+        all_comments_flattened[comment_id] = comment
+        
+for comment_id, comment in all_comments_flattened.items():
+    date = comment["9"]
+    if "year" not in date:
+        date = "0 years ago"
+    if not all_comments_remaster.get(date):
+        all_comments_remaster[date] = {}
+    all_comments_remaster[date][comment_id] = comment
+    
+with open(f"{utils.get_data_path()}/{comment_json_filename}.json", "w") as f:
+    json.dump(all_comments_remaster, f)
--- verify_continuity.py
+++ verify_continuity.py
@@ -17,10 +17,13 @@ def verify_brackets():
     for date_bracket, comments in sorted_all_comments:
         sorted_comments = sorted(comments.items(), key=lambda item: int(item[1]["6"]))
         
-        current_bracket_min = int(sorted_comments[0][1]["6"])
-        current_bracket_max = int(sorted_comments[-1][1]["6"])
-        
-        print(f"Bracket {date_bracket} has {len(sorted_comments)} comments, from {sorted_comments[0][1]['6']} to {sorted_comments[-1][1]['6']}")
+        if len(sorted_comments) > 0:
+            current_bracket_min = int(sorted_comments[0][1]["6"])
+            current_bracket_max = int(sorted_comments[-1][1]["6"])
+        else:
+            continue
+
+        print(f"Bracket {date_bracket} has {len(sorted_comments)} comments, from {current_bracket_min} to {current_bracket_max}")
         # check for overlap
         if last_bracket_max >= current_bracket_min:
             print(f"Overlap detected between brackets {last_bracket_min}-{last_bracket_max} and {current_bracket_min}-{current_bracket_max}")
